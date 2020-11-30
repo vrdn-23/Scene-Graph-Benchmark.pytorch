6f6a68b08f1654f917a7afdcda4db49f8a55df03
Vinay Damodaran <vinaydamodaran95@gmail.com>
2020-11-30 03:24:35 -0500

    debug

--- maskrcnn_benchmark/engine/inference.py
+++ maskrcnn_benchmark/engine/inference.py
@@ -160,7 +160,7 @@ def inference(
 
 def custom_sgg_post_precessing(predictions):
     output_dict = {}
-    print(predictions.keys())
+    print(predictions)
     for idx, boxlist in enumerate(predictions):
         xyxy_bbox = boxlist.convert('xyxy').bbox
         # current sgg info
